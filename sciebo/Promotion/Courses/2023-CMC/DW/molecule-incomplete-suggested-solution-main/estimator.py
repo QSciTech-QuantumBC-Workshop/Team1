7a89ed2ccde277ff31127e2320797450a1f8d103
Jero11 <wagnerd1@myumanitoba.ca>
2023-02-02 20:24:37 +0100

    Dennis Wagner Code

---
+++ sciebo/Promotion/Courses/2023-CMC/DW/molecule-incomplete-suggested-solution-main/estimator.py
@@ -0,0 +1,380 @@
+"""
+estimator.py - To estimate expectation value of observables
+
+Copyright 2020-2023 Maxime Dion <maxime.dion@usherbrooke.ca>
+This file has been modified by <Your,Name> during the
+QSciTech-QuantumBC virtual workshop on gate-based quantum computing.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import time
+
+import numpy as np
+
+from qiskit import QuantumCircuit, execute
+from qiskit.providers import Backend
+
+from pauli_string import PauliString, LinearCombinaisonPauliString
+
+from typing import Union, Optional
+from numpy.typing import NDArray
+from qiskit.quantum_info.operators import Operator
+import sys
+
+
+class Estimator:
+    def __init__(self, varform: QuantumCircuit, backend: Backend, execute_opts={}, record: Optional[object] = None):
+        """
+        An Estimator allows to transform an observable into a callable function. The observable is not set at the 
+        initialization. The estimator will build the QuantumCircuit necessary to estimate the expected value of the 
+        observable. Upon using the 'eval' method, it will execute these circuits and interpret the results to return an
+        estimate of the expected value of the observable.
+
+        Args:
+            varform (QuantumCircuit): A paramatrized QuantumCircuit.
+            backend (Backend): A qiskit backend. Could be a simulator are an actual quantum computer.
+            execute_opts (dict, optional): Optional arguments to be passed to the qiskit.execute function.
+                                           Defaults to {}.
+
+            record (object, optional): And object that could be called on each evaluation to record the results. 
+                Defaults to None.
+        """
+
+        self.varform = varform
+        self.backend = backend
+        self.execute_opts = execute_opts
+
+        self.record = record
+
+        # To be set attributes
+        self.n_qubits = varform.num_qubits
+        self.diagonalizing_circuits = list()
+        self.diagonal_observables = list()
+
+    def set_observable(self, observable: LinearCombinaisonPauliString) -> None:
+        """
+        Set the observable which the expectation value will be estimated.
+        This sets the value of the attribute 'n_qubits'.
+        The observable is converted into a list of diagonal observables along the circuit which performs this diagonalization.
+        This is done using the 'diagonal_observables_and_circuits' method (defined at the subclass level).
+
+        Args:
+            observable (LinearCombinaisonPauliString): The observable to be evaluated.
+        """
+
+        self.diagonal_observables, self.diagonalizing_circuits = self.diagonal_observables_and_circuits(observable)
+
+    def eval(self, params: Union[NDArray, list]) -> float:
+        """
+        Evaluate an estimation of the expectation value of the set observable.
+
+        Args:
+            params (list or NDArray): Parameter values at which the expectation value should be evaluated.
+                Will be fed to the 'varform' paramatrized QuantumCircuit.
+
+        Returns:
+            float: The estimated expectation value of the observable.
+        """
+
+        t0 = time.time()
+        state_circuit = self.prepare_state_circuit(params)
+        circuits = self.assemble_circuits(state_circuit)
+
+        expectation_value = 0
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        # A. For each pair of diagonal observable and circuit :
+        #   1. Execute the circuit on the backend
+        #   2. Extract the result from the job
+        #   3. Estimate the expectation value of the diagonal_observable
+        # B. Combine all the results into the expectation value of the observable (e.i. the energy)
+        # (Optional) record the result with the record object
+        # (Optional) monitor the time of execution
+        ################################################################################################################
+
+        raise NotImplementedError()
+
+        eval_time = time.time()-t0
+
+        return expectation_value
+
+    def prepare_state_circuit(self, params: Union[NDArray, list]) -> QuantumCircuit:
+        """
+        Assign parameter values to the variational circuit (varfom) to prepare the quantum state.
+
+        Args:
+            params (list or NDArray): Params to be assigned to the 'varform' QuantumCircuit.
+
+        Returns:
+            QuantumCircuit: The quantum state circuit
+        """
+
+        state_circuit = QuantumCircuit(self.n_qubits)
+
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        param_dict = dict(zip(self.varform.parameters, params))
+        state_circuit = self.varform.assign_parameters(param_dict) ################################################################################################################
+
+        #raise NotImplementedError()
+
+        return state_circuit
+
+    def assemble_circuits(self, state_circuit: QuantumCircuit) -> list[QuantumCircuit]:
+        """
+        For every diagonal observable, assemble the complete circuit with:
+        - State preparation
+        - Measurement circuit
+        - Measurements
+
+        Args:
+            state_circuit (QuantumCircuit): The quantum state circuit
+
+        Returns:
+            list<QuantumCircuit>: The quantum circuits to be executed.
+        """
+
+        circuits = list()
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        ################################################################################################################
+
+        raise NotImplementedError()
+
+        return circuits
+
+    @staticmethod
+    def diagonal_pauli_string_eigenvalue(diagonal_pauli_string: PauliString, state: str) -> float:
+        """
+        Computes the eigenvalue (+1 or -1) of a diagonal pauli string for a basis state.
+
+        Args:
+            diagonal_pauli_string (PauliString): A diagonal pauli string
+            state (str): a basis state (ex : '1011')
+
+        Returns:
+            float: The eigenvalue
+        """
+
+        eigenvalue = 0
+
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        dps = str(diagonal_pauli_string)
+        eigenvalue = 1
+        count = -1
+        for letter in dps:
+            count += 1
+            if letter == 'Z' and state[count] == '1':
+                eigenvalue *= -1
+        #print(state2)
+        #sys.exit()
+            ################################################################################################################
+
+        #raise NotImplementedError()
+
+        return eigenvalue
+
+    @staticmethod
+    def estimate_diagonal_pauli_string_expectation_value(diagonal_pauli_string: PauliString, counts: dict) -> float:
+        """
+        Estimate the expectation value for a diagonal pauli string based on counts.
+
+        Args:
+            diagonal_pauli_string (PauliString): The diagonal pauli string (must be only I and Z)
+            counts (dict): Contains the number of times each basis state was obtained from a measurement
+
+        Returns:
+            float: The expectation value of the Pauli string
+        """
+
+        expectation_value = 0.0
+
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        #print(diagonal_pauli_string)
+        #print(counts)
+        len_counts = len(counts)
+        Ntot = 0.0
+        count = -1
+        for key, value in counts.items():
+            Ntot += value
+            expectation_value+=value *Estimator.diagonal_pauli_string_eigenvalue(diagonal_pauli_string, key)
+        expectation_value = expectation_value/Ntot    
+    ################################################################################################################
+
+        #raise NotImplementedError()
+
+        return expectation_value
+
+    @staticmethod
+    def estimate_diagonal_observable_expectation_value(diagonal_observable: LinearCombinaisonPauliString,
+                                                       counts: dict) -> float:
+        """
+        Estimate the expectation value for a diagonal observable (linear combinaison of pauli strings) based on counts.
+
+        Args:
+            diagonal_observable (LinearCombinaisonPauliString): The observable (must be only I and Z)
+            counts (dict): Contains the number of times each basis state was obtained from a measurement
+
+        Returns:
+            float: The expectation value of the Observable
+        """
+
+        expectation_value = 0
+
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        ################################################################################################################
+
+        raise NotImplementedError()
+
+        return expectation_value
+
+    @staticmethod
+    def diagonalizing_pauli_string_circuit(pauli_string: PauliString) -> tuple[QuantumCircuit, PauliString]:
+        """
+        Builds the circuit representing the transformation which diagonalizes the given PauliString.
+
+        Args:
+            pauli_string (PauliString): The pauli string to be diagonalized
+
+        Returns:
+            QuantumCircuit: A quantum circuit representing the transformation which diagonalizes the given PauliString
+            PauliString: The diagonal PauliString                   
+        """
+        
+        n_qubits = len(pauli_string)
+        diagonalizing_circuit = QuantumCircuit(n_qubits)
+        diagonal_pauli_string = None
+
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        #I_MAT = np.array([[1, 0], [0, 1]])
+        #X_MAT = np.array([[0, 1], [1, 0]])
+        #Y_MAT = np.array([[0, -1j], [1j, 0]])
+        #Z_MAT = np.array([[1, 0], [0, -1]])
+        #w, V = np.linalg.eig(X_MAT)
+        #D = np.matmul(V,np.matmul(X_MAT,np.linalg.inv(V)))
+        #H = Operator(V)
+        #print(V)
+        #sys.exit()
+        p_str = str(pauli_string)
+        pstr_len = len(p_str)
+        #diagonal_pauli_string = np.zeros(pstr_len, dtype = np.str)
+        diagonal_pauli_string = ''
+        count = -1
+        for letter in p_str:
+            count += 1
+            if letter != 'I':
+                if letter == 'X':
+                   diagonalizing_circuit.h(count)
+                if letter == 'Y':
+                   diagonalizing_circuit.sdg(count)
+                #diagonal_pauli_string[count] = 'Z'
+                diagonal_pauli_string += 'Z' 
+            else:
+                diagonal_pauli_string += 'I'               ################################################################################################################
+        
+        #raise NotImplementedError()
+
+        return diagonalizing_circuit, diagonal_pauli_string
+
+
+class BasicEstimator(Estimator):
+    """
+    The BasicEstimator should build 1 quantum circuit and 1 interpreter for each PauliString.
+    The interpreter should be 1d array of size 2**number of qubits.
+    It does not exploit the fact that commuting PauliStrings can be evaluated from a common circuit.
+    """
+    
+    @staticmethod
+    def diagonal_observables_and_circuits(observable: LinearCombinaisonPauliString) -> tuple[list[LinearCombinaisonPauliString],
+                                                                                             list[QuantumCircuit]]:
+        """
+        This method converts each PauliString in the observable into :
+        - A diagonal observable including the associated coefficient
+        - The quantum circuit that performs this diagonalization.
+        The diagonal observables and the quantum circuits are return as two list.
+
+        Args:
+            observable (LinearCombinaisonPauliString): An observable.
+
+        Returns:
+            list<LinearCombinaisonPauliString> : The diagonal observables (one PauliString long).
+            list<list of QuantumCircuit> : The diagonalizing quantum circuits
+        """
+        
+        diagonal_observables = list()
+        diagonalizing_circuits = list()
+        
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        # Hint : the next method does the work for 1 PauliString + coef
+        ################################################################################################################
+        
+        raise NotImplementedError()
+
+        return diagonal_observables, diagonalizing_circuits
+
+
+class BitwiseCommutingCliqueEstimator(Estimator):
+    """
+    The BitwiseCommutingCliqueEstimator should build 1 quantum circuit and 1 interpreter for each clique of PauliStrings.
+    The interpreter should be 2d array of size (number of cliques ,2**number of qubits).
+    It does exploit the fact that commuting PauliStrings can be evaluated from a common circuit.
+    """
+
+    @staticmethod
+    def diagonal_observables_and_circuits(observable: LinearCombinaisonPauliString) -> tuple[list[LinearCombinaisonPauliString],
+                                                                                             list[QuantumCircuit]]:
+        """
+        This method first divide the observable into bitwise commuting cliques. Each commuting clique is then converted
+        into :
+        - A diagonal observable including the associated coefficients
+        - The quantum circuit that performs this diagonalization.
+        The diagonal observables and the quantum circuits are return as two list.
+
+        Args:
+            observable (LinearCombinaisonPauliString): An observable.
+
+        Returns:
+            list<LinearCombinaisonPauliString> : The diagonal observables
+            list<list of QuantumCircuit> : The diagonalizing quantum circuits
+        """
+
+        cliques = observable.divide_in_bitwise_commuting_cliques()
+
+        diagonal_observables = list()
+        diagonalizing_circuits = list()
+        
+        ################################################################################################################
+        # YOUR CODE HERE
+        # TO COMPLETE (after lecture on VQE)
+        # Hint : the next method does the work for 1 PauliString + coef
+        ################################################################################################################
+        
+        raise NotImplementedError()
+            
+        return diagonal_observables, diagonalizing_circuits
+
+
